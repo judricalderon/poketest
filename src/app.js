afc596aa70477058b3c585047d52f8ed93da3cca
judricalderon <judricalderon@gmail.com>
2026-04-16 19:26:05 -0500

    generamos el app

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const pokemonModule = require('./modules/pokemon/module');
+const errorMiddleware = require('./middlewares/middleware');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/external-data', pokemonModule.router);
+
+app.use(errorMiddleware);
+
+module.exports = app;
